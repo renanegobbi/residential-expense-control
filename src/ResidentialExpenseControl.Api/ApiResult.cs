0b52ecf04fa29053f6b369eab77edcbc8fb18363
Renan Gobbi <renaneg2019@hotmail.com>
2026-02-26 19:09:16 -0300

    feat(api): create ApiResult class to standardize API response forma

---
+++ src/ResidentialExpenseControl.Api/ApiResult.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ResidentialExpenseControl.Domain.Interfaces.Commands.Output;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace ResidentialExpenseControl.Api
+{
+    /// <summary>
+    /// Default result for all API routes
+    /// </summary>
+    public class ApiResult : IActionResult
+    {
+        private readonly IOutput _output;
+
+        public ApiResult(IOutput output)
+        {
+            _output = output;
+        }
+
+        public async Task ExecuteResultAsync(ActionContext context)
+        {
+            var jsonResult = new JsonResult(_output)
+            {
+                StatusCode = !_output.Success
+                    ? (int)HttpStatusCode.BadRequest
+                    : (int)HttpStatusCode.OK
+            };
+
+            await jsonResult.ExecuteResultAsync(context);
+        }
+    }
+}
