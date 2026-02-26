e4fa058a928c50d2c945d6ea87d1041e7409c64b
Renan Gobbi <renaneg2019@hotmail.com>
2026-02-26 19:10:05 -0300

    feat(api): add BadRequestApiResponse for Swagger documentation examples

---
+++ src/ResidentialExpenseControl.Api/ApiResponse.cs
@@ -0,0 +1,21 @@
+﻿using ResidentialExpenseControl.Domain.Commands.Output;
+using Swashbuckle.AspNetCore.Filters;
+
+namespace ResidentialExpenseControl.Api
+{
+    /// <summary>
+    /// Response padrão da API para o erro HTTP 400
+    /// </summary>
+    public class BadRequestApiResponse : Output, IExamplesProvider<Output>
+    {
+        public BadRequestApiResponse()
+            : base(false, new[] { "O campo X é obrigatório e não foi informado.", "O campo Y é obrigatório e não foi informado." }, null)
+        {
+        }
+
+        public Output GetExamples()
+        {
+            return new BadRequestApiResponse();
+        }
+    }
+}
