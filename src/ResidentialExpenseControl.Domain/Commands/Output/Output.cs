751218a62254b00dd1c0e8d17d53ac3f874fa744
Renan Gobbi <renaneg2019@hotmail.com>
2026-02-26 18:53:46 -0300

    feat(domain): introduce Output class for standardized command results

---
+++ src/ResidentialExpenseControl.Domain/Commands/Output/Output.cs
@@ -0,0 +1,21 @@
+﻿using ResidentialExpenseControl.Domain.Interfaces.Commands.Output;
+using System.Collections.Generic;
+
+namespace ResidentialExpenseControl.Domain.Commands.Output
+{
+    public class Output : IOutput
+    {
+        public bool Success { get; }
+
+        public IEnumerable<string> Messages { get; }
+
+        public object Result { get; }
+
+        public Output(bool success, IEnumerable<string> messages, object result)
+        {
+            Success = success;
+            Messages = messages;
+            Result = result;
+        }
+    }
+}
